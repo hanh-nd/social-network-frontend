7fa0f7d5e893e19315cea6c0ac7e69beac7283b4
hanhnd <hanhnd156@gmail.com>
2023-05-09 23:43:01 +0700

    [Feat] fix resizeobserver error

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,19 @@ export default class App extends Vue {
     created(): void {
         const loginUser = localStorageAuthService.getLoginUser();
         appModule.setLoginUser(loginUser);
+
+        window.addEventListener('error', (e) => {
+            if (e.message === 'ResizeObserver loop limit exceeded') {
+                const resizeObserverErrDiv = document.getElementById('webpack-dev-server-client-overlay-div');
+                const resizeObserverErr = document.getElementById('webpack-dev-server-client-overlay');
+                if (resizeObserverErr) {
+                    resizeObserverErr.setAttribute('style', 'display: none');
+                }
+                if (resizeObserverErrDiv) {
+                    resizeObserverErrDiv.setAttribute('style', 'display: none');
+                }
+            }
+        });
     }
 
     get pageName(): RouteRecordName {
